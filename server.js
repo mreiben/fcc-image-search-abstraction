a34e14013b240396dde6859daf58ef0aec0371ee
mreiben <jason.eiben@gmail.com>
2017-01-04 19:57:18 +0000

    added mongodb to track searches

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
 
 //google api key stored in environment variable:
 // 'export VAR_NAME="value"'
 //GOOGLE_API_KEY
+//GOOGLE_CX
+//MONGOLAB_URI
+
+var mongo_url = process.env.MONGOLAB_URI;
 
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
-  key: process.env.GOOGLE_API_KEY,
-  cx: process.env.GOOGLE_CX
+    key: process.env.GOOGLE_API_KEY,
+    cx: process.env.GOOGLE_CX
 });
 
-app.get('/:term', function (req, res) {
+app.get('/', function(req, res){
+   res.send("add '/search/' with a term and an optional offset to the url above, such as '/search/flipper?offset=5'"); 
+});
+
+app.get('/search/:term', function (req, res) {
     var term = req.params.term;
+    var offset = req.query.offset;
+    
+    //connect to mongodb through mlab
+    mongo.connect(mongo_url, function(err, db){
+            if (err) throw err;
+            
+            var currentdate = new Date();
+            var datetime = currentdate.getDay() + "/"+ (currentdate.getMonth()+1) 
+                + "/" + currentdate.getFullYear() + " @ " 
+                + currentdate.getHours() + ":" 
+                + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+            
+            var doc = { term: term, when: datetime }
+            
+            //add doc with search term and current timestamp
+            var records = db.collection('record');
+            records.insert(doc, function(err, data){
+                if (err) throw err;
+                console.log(JSON.stringify(doc));
+            })
+            
+            db.close();
+        });
+    
     googleSearch.build({
         q: term,
-        num: 10
+        num: parseInt(offset),
     }, function(error, response) {
-        var res_arr = Object.keys(response).map(function(k) { return response[k] });
+        var res_arr = Object.keys(response).map(function(k) { return response[k]; });
         var json_data = {};
         
         var i = 1;
@@ -35,6 +68,26 @@ app.get('/:term', function (req, res) {
         
         console.log(json_data);
         res.send(json_data);
+    });
+});
+
+//add get method for getting latest requests to app
+app.get('/latest', function (req, res) {
+    //connenct to db
+    mongo.connect(mongo_url, function(err, db){
+        if (err) throw err;
+        console.log("connecting to database...");
+
+        //find all records
+        var records = db.collection('record');
+        console.log("found records:");
+        //find all records and add to data
+        var all_records = records.find({},{"term":1, "when":1, "_id":0}).toArray(function(err, all_records){
+            if (err) throw err;
+            console.log(all_records);
+            res.send(all_records.reverse());
+            db.close();
+        });
     });
 });
 
